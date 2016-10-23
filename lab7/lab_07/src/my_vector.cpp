7421a6a929080da9e6e2c402837cdfb0f4bd4b68
123a6bcw <ga.karandash@yandex.ru>
2016-10-23 21:00:41 +0300

    fixed some bad style thing

--- lab7/lab_07/src/my_vector.cpp
+++ lab7/lab_07/src/my_vector.cpp
@@ -3,21 +3,24 @@
 MyVector::MyVector()
 {
     this -> _data = new int[2];
+    *(_data) = 0;
+    *(_data + 1) = 0;
+    
     this -> _sz = 0;
     this -> _cp = 2;
 }
 
 MyVector::MyVector(std::size_t init_capacity)
 {
-    this -> _data = new int[init_capacity];
+    (this -> _data) = new int[init_capacity];
     
     for (std::size_t i = 0; i < init_capacity; i++)
     {
         *((this -> _data) + i) = 0;
     }
     
-    this -> _cp = init_capacity;
-    this -> _sz = 0;
+    (this -> _cp) = init_capacity;
+    (this -> _sz) = 0;
 }
 
 MyVector::~MyVector()
@@ -27,22 +30,22 @@ MyVector::~MyVector()
 
 void MyVector::set(std::size_t index, int value)
 {
-    *(this -> _data + index) = value; 
+    *((this -> _data) + index) = value; 
 }
 
 int MyVector::get(std::size_t index)
 {
-    return *(this -> _data + index);
+    return *((this -> _data) + index);
 }
 
 std::size_t MyVector::size()
 {
-    return this -> _sz;
+    return (this -> _sz);
 }
 
 std::size_t MyVector::capacity()
 {
-    return this -> _cp;
+    return (this -> _cp);
 }
 
 void MyVector::reserve(std::size_t new_capacity)
@@ -55,15 +58,15 @@ void MyVector::reserve(std::size_t new_capacity)
     
     for (std::size_t i = (this -> _sz); i < new_capacity; i++)
     {
-        *((_data_new) + i) = 0;
+        *(_data_new + i) = 0;
     }
     
     delete [] (this -> _data);
-    this -> _data = _data_new; 
+    (this -> _data) = _data_new; 
     
     if (new_capacity > (this -> _cp))
     {
-        this -> _cp = new_capacity;
+        (this -> _cp) = new_capacity;
     }
 }
 
@@ -71,13 +74,13 @@ void MyVector::resize(std::size_t new_size)
 {
     if (new_size < (this -> _sz))
     {
-        this -> _sz = new_size;
+        (this -> _sz) = new_size;
         return;
     }
     
     if (new_size > (this -> _cp))
     {
-        std::size_t _new_capacity = this -> _cp;
+        std::size_t _new_capacity = (this -> _cp);
         while (_new_capacity < new_size)
         {
             _new_capacity *= 2;
@@ -86,7 +89,7 @@ void MyVector::resize(std::size_t new_size)
         this -> reserve(_new_capacity);
     }
     
-    this -> _sz = new_size;
+    (this -> _sz) = new_size;
 }
 
 void MyVector::push_back(int value)
@@ -126,7 +129,7 @@ void MyVector::erase(std::size_t index)
 { 
     for (std::size_t i = index + 1; i <= (this -> _sz) - 1; i++)
     {
-        (*((this -> _data) + i - 1)) = (*((this -> _data) + i));
+        *((this -> _data) + i - 1) = *((this -> _data) + i);
     }
     
     (this -> _sz)--;
